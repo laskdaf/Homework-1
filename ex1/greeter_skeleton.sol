32143c6487dee5b4412d856969a6e180438b2eae
kevin <kevinc_2019@berkeley.edu>
2017-09-14 01:52:43 -0700

    finished hw1

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -3,13 +3,40 @@ pragma solidity ^0.4.13;
 
 contract greeter {
 	/* Add one variable to hold our greeting */
-	string greeting;
+	uint x;
+	string [] greetings;
+	uint [] greetingCounts;
+	uint count;
 
 	function greeter(string _greeting) public {
 		/* Write one line of code for the contract to set our greeting */
+		greetings = new string [](3);
+		greetingCounts = new uint[](3);
+
+		if (bytes(_greeting).length == 0) {
+		    greetings[0] = "Greetings";
+		} else {
+		    greetings[0] = _greeting;
+		}
+		greetings[1] = "Hello World!";
+		greetings[2] = "B@B #1";
+
+		greetingCounts[0] = 0;
+		greetingCounts[1] = 0;
+		greetingCounts[2] = 0;
+
+		count = 0;
 	}
 
-	function greet() constant returns (string)  {
+    // Returns a tuple of the # of times a specific greeting has been called as well as that greeting
+	function greetCycle() returns (uint, string)  { // Removed constant delimeter to increment the counter
 		/* Write one line of code to allow the contract to return our greeting */
+		uint index = count % 3;
+		greetingCounts[index] = greetingCounts[index] + 1;
+		count += 1;
+		return (greetingCounts[index], greetings[index]);
 	}
+
+	function() { x = 1; }
+
 }
--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -2,9 +2,45 @@ pragma solidity ^0.4.16;
 
 
 contract Fibonacci {
+    uint x;
+
+    mapping(uint => uint) fib;
+    uint lastfib;
+
+    function Fibonacci () {
+        fib[1] = 1;
+        fib[2] = 1;
+        lastfib = 2;
+    }
+
+    event ReturnFibonacci(uint);
+    // event SavedValue(string);
 	function calculate(uint position) returns (uint result) {
 		/* Add one variable to hold our greeting */
+		if (fib[position] != 0) {
+		    ReturnFibonacci(fib[position]);
+		  //  SavedValue("This value has already been calculated.");
+		    return fib[position];
+		}
+
+		uint a = fib[lastfib - 1];
+		uint b = fib[lastfib];
+
+		uint count = lastfib;
+		uint temp = 0;
+
+		while (count < position) {
+		    temp = a + b;
+		    a = b;
+		    b = temp;
+		    count += 1;
+		    fib[count] = b;
+		}
+		lastfib = count;
+		ReturnFibonacci(fib[position]);
+		return fib[position];
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function() { x = 1; }
 }
